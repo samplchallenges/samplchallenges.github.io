ef2f56c7c18b502e157498fd7c966ae8ece58daf
megosato <osatom@uci.edu>
2022-02-07 12:16:22 -0800

    add auto reference pull workflow

--- _history/references_header.md
+++ _history/references_header.md
@@ -1,10 +1,10 @@
----
-layout: single
-sidebar:
-  nav: references.md
-title: Links to SAMPL special issues
-excerpt: Here are links to key SAMPL information and special issues
-permalink: /history/references/
+[//]: #---
+[//]: #layout: single
+[//]: #sidebar:
+[//]: #  nav: references.md
+[//]: #title: Links to SAMPL special issues
+[//]: #excerpt: Here are links to key SAMPL information and special issues
+[//]: #permalink: /history/references/
 ---
 
 The various SAMPL challenges are described in:
@@ -29,19 +29,3 @@ The various SAMPL challenges are described in:
 ## Additional references
 
 A full list of some 100 SAMPL references we previously identified is also available in the [SAMPL proposal](http://escholarship.org/uc/item/7cf8c6cr); we plan to propagate a complete reference list to this website at some point. Typically all SAMPL papers cite "SAMPLN" in their titles, where N is a number, e.g. "SAMPL7", making these easy to discover via Google Scholar, Web of Science or similar search engines.
-
-1. **Temperature artifacts in protein structures bias ligand-binding predictions**
-Shanshan YC Bradford, Léa El Khoury, Yunhui Ge, Meghan Osato, David L Mobley, Marcus Fischer *Chemical Science* (2021) <https://doi.org/gmjgtj>
-DOI: [10.1039/d1sc02751d](https://doi.org/10.1039/d1sc02751d) · PMID: [34667539](https://www.ncbi.nlm.nih.gov/pubmed/34667539) · PMCID: [PMC8447925](https://www.ncbi.nlm.nih.gov/pmc/articles/PMC8447925)
-
-2. **JPlogP: an improved logP predictor trained using predicted data**
-Jeffrey Plante, Stephane Werner *Journal of Cheminformatics* (2018-12) <https://doi.org/gfw677>
-DOI: [10.1186/s13321-018-0316-5](https://doi.org/10.1186/s13321-018-0316-5) · PMID: [30552535](https://www.ncbi.nlm.nih.gov/pubmed/30552535) · PMCID: [PMC6755606](https://www.ncbi.nlm.nih.gov/pmc/articles/PMC6755606)
-
-3. **Computational prediction of octanol–water partition coefficient based on the extended solvent-contact model**
-Taeho Kim, Hwangseo Park *Journal of Molecular Graphics and Modelling* (2015-07) <https://doi.org/f7m285>
-DOI: [10.1016/j.jmgm.2015.06.004](https://doi.org/10.1016/j.jmgm.2015.06.004) · PMID: [26142695](https://www.ncbi.nlm.nih.gov/pubmed/26142695)
-
-4. **The SAMPL4 host–guest blind prediction challenge: an overview**
-Hari S Muddana, Andrew T Fenley, David L Mobley, Michael K Gilson *Journal of Computer-Aided Molecular Design* (2014-03-06) <https://doi.org/f5585s>
-DOI: [10.1007/s10822-014-9735-1](https://doi.org/10.1007/s10822-014-9735-1) · PMID: [24599514](https://www.ncbi.nlm.nih.gov/pubmed/24599514) · PMCID: [PMC4053502](https://www.ncbi.nlm.nih.gov/pmc/articles/PMC4053502)
---
+++ generate_references.py
@@ -0,0 +1,104 @@
+# generate_references.py
+
+# pip install pyzotero
+from pyzotero import zotero
+import subprocess
+import argparse
+import os
+
+def create_markdown_file_with_header(tempfile: str, outfile: str, headerfile: str):
+	with open(outfile, 'w') as outf:
+		with open(headerfile, 'r') as headerf:
+			for line in headerf:
+				if line.startswith("[//]: #"):
+					outf.write(line[7:])
+				else:
+					outf.write(line)
+			outf.write("\n\n")
+		with open(tempfile, 'r') as tempf:
+			for line in tempf:
+				outf.write(line)
+
+def get_manubot_command(outfile: str, string_of_idenfiers: str):
+    command = "manubot cite --format=markdown "
+    if outfile:
+        command += f"--output={outfile} "
+    return command + string_of_identifiers
+
+def get_doi(item):
+    if "DOI" not in item["data"].keys():
+        return None
+    if item["data"]["DOI"] == "":
+        return None
+    if "," in item["data"]["DOI"]:
+        return "doi:" + item["data"]["DOI"].split(",")[0]
+    else:
+        return "doi:" + item["data"]["DOI"]
+
+def get_isbn(item):
+    if "ISBN" not in item["data"].keys():
+        return None
+    else:
+        return "isbn:" + item["data"]["ISBN"]
+
+
+def get_url(item):
+    if "url" not in item["data"].keys():
+        return None
+    else:
+        return "url:" + item["data"]["url"]
+
+if __name__ == "__main__":
+    
+    ap = argparse.ArgumentParser()
+    ap.add_argument("-t", "--tag", help="Zotero library tag", required=False)
+    ap.add_argument("--lib-id", help="Zotero user ID from https://www.zotero.org/settings/keys", required=True)
+    ap.add_argument("--api-key", help="Zotero API key from https://www.zotero.org/settings/keys/new", required=True)
+    ap.add_argument("--outfile", help="Filename for Markdown output", required=False)
+    ap.add_argument("--headerfile", help="Header file for Markdown output", required=False)
+
+    args = vars(ap.parse_args())
+    
+
+    library_type = "user"
+    zot = zotero.Zotero(args["lib_id"], library_type, args["api_key"])
+
+    if args["tag"]:
+        zot.add_parameters(tag=args["tag"], sort="date")
+    else:
+        raise NotImplementedError
+
+    items = zot.items()
+    string_of_identifiers = ""
+
+    for item in items:
+    	title = item["data"]["title"]
+
+    	identifer = get_doi(item)
+    	if not identifer:
+    		identifer = get_isbn(item)
+    	if not identifer:
+    		identifer = get_url(item)
+    	if not identifer:
+        	print(f"{title[0:30]:<40}... Error finding DOI, ISBN, or URL for item.")
+        	continue
+    	string_of_identifiers += identifer
+    	string_of_identifiers += " "
+
+    ofile = None
+    tempfile = "temp.md"
+    if args["outfile"] and args["headerfile"]:
+        ofile = tempfile
+    if args["outfile"] and not args["headerfile"]:
+        ofile = args["outfile"]
+
+    command = get_manubot_command(ofile, string_of_identifiers)
+    print(command)
+
+    subprocess.call(command, shell=True)
+
+    # if a header is specified, create a markdown file with header
+    if args["outfile"] and args["headerfile"]:
+        create_markdown_file_with_header(ofile, args["outfile"], args["headerfile"])
+        if os.path.isfile(ofile):
+            os.remove(ofile)
